410d84bf17b54ec356c532edb6ef82518d7e40dc
laurent <lgibe.dev@gmail.com>
2018-04-07 17:44:00 +0000

    plant.yaml gardener purchase

--- src/Entity/Plot.php
+++ src/Entity/Plot.php
@@ -7,9 +7,69 @@
  */
 
 namespace App\Entity;
-
-
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Ramsey\Uuid\Uuid;
 class Plot
 {
+    /**
+     * The identifier of the Plot
+     *
+     * @var string
+     * @ORM\Column(name="id", type="string")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id = null;
+
+    /**
+     * The creation date of the plot.
+     *
+     * @var \DateTime
+     * @ORM\Column(type="datetime", name="created_at")
+     */
+    private $createdAt = null;
+
+
+    public function __construct()
+    {
+
+        $this->createdAt = new \DateTime();
+        $this->id = Uuid::uuid4()->toString();
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
 
 }
